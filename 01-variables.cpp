2dd35f26393ae951c00ec6a8455417d60ce5b97d
luckyyoseph-cmd <krishmasih1023@gmail.com>
2026-09-04 14:59:12 +0530

    Day 02: Basic Practice Problems

--- 01-variables.cpp
+++ 01-variables.cpp
@@ -3,16 +3,73 @@ using namespace std;
 
 int main() {
 
-    // Variables
-    int age = 18;
-    float marks = 85.5;
-    char grade = 'A';
-    bool isStudent = true;
+    // ========================================
+    // Q1. Area of a Square
+    // ========================================
 
-    cout << "Age: " << age << endl;
-    cout << "Marks: " << marks << endl;
-    cout << "Grade: " << grade << endl;
-    cout << "Student: " << isStudent << endl;
+    float side;
+
+    cout << "Enter the side of square: ";
+    cin >> side;
+
+    float squareArea = side * side;
+
+    cout << "Area of square = " << squareArea << endl;
+
+
+    // ========================================
+    // Q2. Total Cost of 3 Items
+    // ========================================
+
+    float pencil, pen, eraser;
+
+    cout << "\nEnter cost of pencil: ";
+    cin >> pencil;
+
+    cout << "Enter cost of pen: ";
+    cin >> pen;
+
+    cout << "Enter cost of eraser: ";
+    cin >> eraser;
+
+    float totalCost = pencil + pen + eraser;
+
+    cout << "Total cost = " << totalCost << endl;
+
+
+    // ========================================
+    // Q3. Simple Interest Calculator
+    // ========================================
+
+    float principal, rate, time;
+
+    cout << "\nEnter principal amount: ";
+    cin >> principal;
+
+    cout << "Enter rate of interest: ";
+    cin >> rate;
+
+    cout << "Enter time: ";
+    cin >> time;
+
+    float simpleInterest = (principal * rate * time) / 100;
+
+    cout << "Simple Interest = " << simpleInterest << endl;
+
+
+    // ========================================
+    // Q4. Area of a Circle
+    // ========================================
+
+    float radius;
+
+    cout << "\nEnter radius of circle: ";
+    cin >> radius;
+
+    float circleArea = 3.14159 * radius * radius;
+
+    cout << "Area of circle = " << circleArea << endl;
+
 
     return 0;
 }
